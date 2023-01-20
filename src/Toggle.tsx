b669a6590a7c6a69e85b7b53893ffd25039ef7a7
Henry Guy Kemp <henry@ticaboo.co.uk>
2023-01-20 17:32:14 +0000

    pre vanilla extract

--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -1,12 +1,20 @@
-import React, { HTMLAttributes, ReactNode } from 'react';
+import React, { InputHTMLAttributes, ReactNode } from 'react';
+// import * as css from './toggleSwitch.css';
 //ts to boilerplate tse?
 
-export interface Props extends HTMLAttributes<HTMLButtonElement> {
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   children: ReactNode;
   variant: 'primary' | 'secondary';
+  id: string;
+  label: string;
 }
 //or? interface Props = {};
 
-export const Toggle = ({ children, variant, ...props }: Props) => {
-  return <button {...props}>{children}</button>;
+export const Toggle = ({ id }: Props) => {
+  return (
+    <div className="toggler">
+      <input type="checkbox" id={id} />
+      <label htmlFor={id}>Toggle</label>
+    </div>
+  );
 };
--- stories/Toggle.stories.tsx
+++ stories/Toggle.stories.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { Toggle, Props } from '../src/Toggle';
 
 const meta: Meta = {
-  title: 'Toggle switch button',
+  title: 'Toggle',
   component: Toggle,
-  //   argTypes: {
-  //     children: {
-  //       control: {
-  //         type: 'text',
-  //       },
-  //     },
-  //   },
-  //   parameters: {
-  //     controls: { expanded: true },
-  //   },
+  argTypes: {
+    variant: { defaultValue: 'primary' },
+    id: { defaultValue: 'ident1' },
+    onClick: { action: 'clicked' },
+  },
 };
 
 export default meta;
 
-//const Template: Story<Props> = (args) => <Toggle {...args} />;
-const Template: Story<Props> = (args) => (
-  <Toggle variant="primary">click me</Toggle>
-);
+const Template: Story<Props> = (args) => <Toggle {...args} />;
 
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
+Default.args = { variant: 'primary', children: 'Default' };
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'secondary',
+  children: 'Secondary',
+  onClick: action('2ndry click'),
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = { variant: 'primary', children: 'tertiary' };
 
 Default.args = {};
